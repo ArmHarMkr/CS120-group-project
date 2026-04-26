279988367fe9274c77c00d9f28ff3cb648c83471
Harutyun Mkrtchyan <harutyun.mkrtchyan2006@gmail.com>
2026-04-26 19:42:57 +0400

    Changing GameMap, GameManager, adding Main for simple console work

--- src/aua/Core/GameManager.java
+++ src/aua/Core/GameManager.java
@@ -1,21 +1,37 @@
 package aua.Core;
 
+import java.util.Scanner;
+
 public class GameManager {
-    private static final int width = 700;
-    private static final int height = 700;
+    private static final int width = 21;
+    private static final int height = 21;
     private GameMap map;
     private Player player;
+    private Point playerPosition;
+    private boolean isRunning;
+    private String message;
 
     public GameManager(String[] storedGame){
-
+        this();
     }
 
     public GameManager(){
         this.player = new Player();
         this.map = new GameMap(width, height);
+        this.playerPosition = findStartingPosition();
+        this.isRunning = true;
+        this.message = "Use roads to move around the map.";
     }
 
     public void start(){
+        Scanner scanner = new Scanner(System.in);
+
+        while(isRunning){
+            draw();
+            System.out.print("Move with W/A/S/D, or Q to quit: ");
+            String command = scanner.nextLine();
+            update(command);
+        }
 
     }
 
@@ -32,6 +48,59 @@ public class GameManager {
     }
 
     public void draw(){
+        System.out.println(map.draw(playerPosition.getX(), playerPosition.getY()));
+        System.out.println("@ player, . road, # rock");
+        System.out.println(message);
+    }
 
+    private void update(String command){
+        if(command == null || command.isEmpty()){
+            return;
+        }
+
+        char input = Character.toLowerCase(command.charAt(0));
+        int nextX = playerPosition.getX();
+        int nextY = playerPosition.getY();
+
+        if(input == 'q'){
+            isRunning = false;
+            message = "Thanks for playing.";
+            return;
+        } else if(input == 'w'){
+            nextY--;
+        } else if(input == 's'){
+            nextY++;
+        } else if(input == 'a'){
+            nextX--;
+        } else if(input == 'd'){
+            nextX++;
+        } else {
+            message = "Unknown command.";
+            return;
+        }
+
+        movePlayer(nextX, nextY);
+    }
+
+    private void movePlayer(int nextX, int nextY){
+        if(map.isWalkable(nextX, nextY)){
+            playerPosition.setX(nextX);
+            playerPosition.setY(nextY);
+            message = "Moved to (" + nextX + ", " + nextY + ").";
+        } else {
+            message = "You cannot walk there.";
+        }
+    }
+
+    private Point findStartingPosition(){
+        for(int y = 0; y < map.getHeight(); y++){
+            for(int x = 0; x < map.getWidth(); x++){
+                if(map.isWalkable(x, y)){
+                    return new Point(x, y);
+                }
+            }
+        }
+
+        return new Point();
     }
 }
