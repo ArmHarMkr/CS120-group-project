aae18579c903bfdbcd0cf899b62f4c3f42dca104
Suren Poghosyan <suren.poghosian.18@gmail.com>
2026-05-02 16:02:20 +0400

    add basic logic for save method according to the current code and data structure

--- src/aua/CLI/Main.java
+++ src/aua/CLI/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
         GameManager gameManager = new GameManager();
         gameManager.start();
+        try {
+            gameManager.save();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
